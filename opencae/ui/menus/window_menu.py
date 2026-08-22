ef8b85c016dfa5b9b34ba4bedc397d42c3b6ad17
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-22 22:41:31 +0200

    Remove Properties from Window menu

--- opencae/ui/menus/window_menu.py
+++ opencae/ui/menus/window_menu.py
@@ -1,6 +1,10 @@
 from opencae.ui.actions.ids import A
 
+
 def build(menu_bar, actions):
-    menu=menu_bar.addMenu('&Window')
-    menu.addAction(actions.get(A.SHOW_PROJECT)); menu.addAction(actions.get(A.SHOW_PROPERTIES)); menu.addAction(actions.get(A.SHOW_OUTPUT)); menu.addSeparator(); menu.addAction(actions.get(A.RESET_LAYOUT))
+    menu = menu_bar.addMenu("&Window")
+    menu.addAction(actions.get(A.SHOW_PROJECT))
+    menu.addAction(actions.get(A.SHOW_OUTPUT))
+    menu.addSeparator()
+    menu.addAction(actions.get(A.RESET_LAYOUT))
     return menu
