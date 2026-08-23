74e18e87d8c5aac28988103e2757664547449bd1
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-23 07:46:04 +0200

    Export split ReferenceUse value object

--- opencae/model/core/__init__.py
+++ opencae/model/core/__init__.py
@@ -1,24 +1,47 @@
+"""Curated public exports for OpenCAE model-core infrastructure."""
+
 from .deck_writer import DeckWriter
 from .entity import Entity
 from .export_context import ExportContext
 from .export_names import ExportNameRegistry, safe_solver_name
 from .model_codec import decode_model, encode_model
 from .model_registry import register_model_type
-from .project_index import ProjectIndex, ReferenceUse
+from .project_index import ProjectIndex
 from .reference import EntityRef, as_entity_ref
+from .reference_use import ReferenceUse
 from .solver_name import SolverName
 from .solver_writable import SolverWritable
 
 __all__ = [
-    "DeckWriter", "Entity", "ExportContext", "ExportNameRegistry", "safe_solver_name", "SolverName", "SolverWritable",
-    "decode_model", "encode_model", "register_model_type", "ProjectIndex", "ReferenceUse",
-    "EntityRef", "as_entity_ref",
-    "clone_entity_graph", "compatible_replacements",
-    "delete_entity_graph", "entity_with_replaced_references", "remap_entity_graph",
-    "remove_entity", "replace_references",
+    "DeckWriter",
+    "Entity",
+    "ExportContext",
+    "ExportNameRegistry",
+    "safe_solver_name",
+    "SolverName",
+    "SolverWritable",
+    "decode_model",
+    "encode_model",
+    "register_model_type",
+    "ProjectIndex",
+    "ReferenceUse",
+    "EntityRef",
+    "as_entity_ref",
+    "clone_entity_graph",
+    "compatible_replacements",
+    "delete_entity_graph",
+    "entity_with_replaced_references",
+    "remap_entity_graph",
+    "remove_entity",
+    "replace_references",
 ]
 
 from .reference_edit import (
-    clone_entity_graph, compatible_replacements, delete_entity_graph, entity_with_replaced_references,
-    remap_entity_graph, remove_entity, replace_references,
+    clone_entity_graph,
+    compatible_replacements,
+    delete_entity_graph,
+    entity_with_replaced_references,
+    remap_entity_graph,
+    remove_entity,
+    replace_references,
 )
