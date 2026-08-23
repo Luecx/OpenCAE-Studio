521bd27f9d67ee2cd0c2b8acf3ad9f27822c4c64
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-23 07:38:52 +0200

    Export typed Job and Result states

--- opencae/model/entities/jobs/__init__.py
+++ opencae/model/entities/jobs/__init__.py
@@ -1,5 +1,17 @@
+"""Public exports for persistent Job and Result entities."""
+
 from .job import Job
+from .job_source_kind import JobSourceKind
+from .job_status import JobStatus
 from .result_field import ResultField
 from .result_set import ResultSet
+from .result_status import ResultStatus
 
-__all__ = ["Job", "ResultField", "ResultSet"]
+__all__ = [
+    "Job",
+    "JobSourceKind",
+    "JobStatus",
+    "ResultField",
+    "ResultSet",
+    "ResultStatus",
+]
