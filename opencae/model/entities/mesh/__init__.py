67f0ee682c6adf4bfef1cc8c0fa2f17cc2697d41
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-23 07:48:42 +0200

    Export split mesh element control types

--- opencae/model/entities/mesh/__init__.py
+++ opencae/model/entities/mesh/__init__.py
@@ -3,7 +3,9 @@
 from .default_seed import DefaultSeed
 from .edge_seed import EdgeSeed
 from .element_block import ElementBlock
-from .element_control import ElementControl, ElementOrder, ElementTopology
+from .element_control import ElementControl
+from .element_order import ElementOrder
+from .element_topology import ElementTopology
 from .mesh_settings import MeshSettings
 from .mesh_state import MeshState
 from .mesh_status import MeshStatus
