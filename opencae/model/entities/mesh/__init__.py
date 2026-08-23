4d230b92d467d185148caa28f63ad760c974d58f
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-23 07:40:04 +0200

    Export typed mesh lifecycle state

--- opencae/model/entities/mesh/__init__.py
+++ opencae/model/entities/mesh/__init__.py
@@ -1,14 +1,25 @@
+"""Public exports for mesh configuration, storage, and lifecycle state."""
+
 from .default_seed import DefaultSeed
 from .edge_seed import EdgeSeed
 from .element_block import ElementBlock
 from .element_control import ElementControl, ElementOrder, ElementTopology
 from .mesh_settings import MeshSettings
-from .node_table import NodeTable
 from .mesh_state import MeshState
+from .mesh_status import MeshStatus
+from .node_table import NodeTable
 from .seed import Seed
 
 __all__ = [
-    "DefaultSeed", "EdgeSeed", "ElementBlock", "ElementControl",
-    "ElementOrder", "ElementTopology", "MeshSettings", "MeshState",
-    "NodeTable", "Seed",
+    "DefaultSeed",
+    "EdgeSeed",
+    "ElementBlock",
+    "ElementControl",
+    "ElementOrder",
+    "ElementTopology",
+    "MeshSettings",
+    "MeshState",
+    "MeshStatus",
+    "NodeTable",
+    "Seed",
 ]
