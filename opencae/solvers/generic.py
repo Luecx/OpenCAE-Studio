6dd2ec0b6570e0540f5e8a2c76fe30e2e1ca8842
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-26 07:32:34 +0200

    Accept deck profile in solver adapters

--- opencae/solvers/generic.py
+++ opencae/solvers/generic.py
@@ -8,5 +8,6 @@ from .base import SolverAdapter
 class GenericAdapter(SolverAdapter):
     name = "Generic"
 
-    def write_deck_text(self, project, analysis):
+    def write_deck_text(self, project, analysis, profile=None):
+        """Render the generic diagnostic deck."""
         return project.render_deck(SolverName.GENERIC, analysis)
