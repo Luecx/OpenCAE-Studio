57e37372712c4c7fe905ac6cc22b99152ba2ab4f
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-28 07:51:16 +0200

    List workspace docks separately in Window menu

--- opencae/ui/menus/window_menu.py
+++ opencae/ui/menus/window_menu.py
@@ -4,7 +4,10 @@ from opencae.ui.actions.ids import A
 def build(menu_bar, actions):
     menu = menu_bar.addMenu("&Window")
     menu.addAction(actions.get(A.SHOW_PROJECT))
-    menu.addAction(actions.get(A.SHOW_OUTPUT))
+    menu.addSeparator()
+    menu.addAction(actions.get(A.SHOW_JOBS))
+    menu.addAction(actions.get(A.SHOW_LOG))
+    menu.addAction(actions.get(A.SHOW_TIME_MANAGER))
     menu.addSeparator()
     menu.addAction(actions.get(A.RESET_LAYOUT))
     return menu
