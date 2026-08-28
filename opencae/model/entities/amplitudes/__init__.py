632d31e1af1c606c147b620a966d275998be704b
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-28 14:49:30 +0200

    Add amplitude model package

---
+++ opencae/model/entities/amplitudes/__init__.py
@@ -0,0 +1,19 @@
+from .amplitude import (
+    Amplitude,
+    FUNCTION_DEFAULTS,
+    FUNCTION_TYPES,
+    INTERPOLATIONS,
+    TIME_BASES,
+    preview_points,
+    sample_function,
+)
+
+__all__ = [
+    "Amplitude",
+    "FUNCTION_DEFAULTS",
+    "FUNCTION_TYPES",
+    "INTERPOLATIONS",
+    "TIME_BASES",
+    "preview_points",
+    "sample_function",
+]
