7631fa96b463c48e33e7fd9d5b35242289cb92f5
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-08-28 14:57:24 +0200

    Add amplitude group to boundary ribbon

--- opencae/ui/ribbon/bc_page.py
+++ opencae/ui/ribbon/bc_page.py
@@ -6,4 +6,5 @@ def groups():
     return (
         RibbonGroupSpec("SUPPORTS", (A.FIXED, A.DISPLACEMENT, A.SYMMETRY)),
         RibbonGroupSpec("LOADS", (A.CLOAD, A.DLOAD, A.PRESSURE, A.VLOAD, A.INERTIA_LOAD, A.TEMPERATURE)),
+        RibbonGroupSpec("AMPLITUDES", (A.AMPLITUDE,)),
     )
