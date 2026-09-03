de46f48be615dcc046a204b1e178d06c7b2c93a4
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-03 15:46:29 +0200

    Export connector constraint

--- opencae/model/entities/constraints/__init__.py
+++ opencae/model/entities/constraints/__init__.py
@@ -1,4 +1,5 @@
 from .base import Constraint
+from .connector import CONNECTOR_TYPES, ConnectorConstraint
 from .distributing_coupling import DistributingCoupling
 from .equation import EquationConstraint, EquationTerm
 from .factory import create_constraint
@@ -10,7 +11,7 @@ from .tie import TieConstraint
 from .types import ConstraintType
 
 __all__ = [
-    "Constraint", "ConstraintType",
+    "Constraint", "ConstraintType", "CONNECTOR_TYPES", "ConnectorConstraint",
     "DistributingCoupling", "EquationConstraint", "EquationTerm", "KinematicCoupling", "MPCConstraint",
     "RigidBodyConstraint", "TieConstraint", "create_constraint", "direct_control_point_error",
 ]
