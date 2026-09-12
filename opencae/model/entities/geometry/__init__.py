1e69c61b800b4ddd3ae08155dd6b676ca0c25148
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-12 11:41:29 +0200

    Export sketch geometry entities

--- opencae/model/entities/geometry/__init__.py
+++ opencae/model/entities/geometry/__init__.py
@@ -5,9 +5,23 @@ from .partition_cell import PartitionCellFeature
 from .partition_edge import PartitionEdgeFeature
 from .partition_face import PartitionFaceFeature
 from .partition_plane import PartitionPlaneFeature
+from .sketch import (
+    SKETCH_ENTITY_TYPES,
+    SketchArc,
+    SketchCircle,
+    SketchConstraint,
+    SketchDefinition,
+    SketchEllipse,
+    SketchFeature,
+    SketchLine,
+    SketchPoint,
+    SketchSpline,
+)
 
 __all__ = [
     "GeometryFeature", "GeometrySettings", "ImportedStepFeature",
     "PartitionCellFeature", "PartitionEdgeFeature", "PartitionFaceFeature",
-    "PartitionPlaneFeature",
+    "PartitionPlaneFeature", "SketchPoint", "SketchLine", "SketchCircle",
+    "SketchArc", "SketchEllipse", "SketchSpline", "SketchConstraint",
+    "SketchDefinition", "SketchFeature", "SKETCH_ENTITY_TYPES",
 ]
