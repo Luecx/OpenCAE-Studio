419dcb7bdfd732be05c743269d988abc609e9083
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-13 19:43:49 +0200

    Export constraint-complete Sketcher dialog

--- opencae/ui/sketcher/__init__.py
+++ opencae/ui/sketcher/__init__.py
@@ -1,7 +1,7 @@
 """Parametric sketch editing workspace."""
 
 from .canvas import SketchCanvas
-from .dialog import SketchFeatureDialog
+from .constraint_dialog import SketchFeatureDialog
 from .preview import SketchFeaturePreview
 
 __all__ = ["SketchCanvas", "SketchFeatureDialog", "SketchFeaturePreview"]
