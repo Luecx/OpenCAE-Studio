ab80ba0195d44f20f86b7812406b996f77263dd9
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-14 21:23:57 +0200

    Remove transitional input aliases

--- opencae/ui/primitives/inputs/__init__.py
+++ opencae/ui/primitives/inputs/__init__.py
@@ -1,6 +1,5 @@
 """Canonical low-level input controls used by reusable OpenCAE composites."""
 
-# Canonical structurally named primitives -----------------------------------
 from .input_form_integer import InputFormInteger
 from .input_form_multiline import InputFormMultiline
 from .input_form_number import InputFormNumber
@@ -8,13 +7,6 @@ from .input_form_text import InputFormText
 from .input_matrix_number import InputMatrixNumber
 from .input_search import InputSearch
 from .input_time_manager_speed import InputTimeManagerSpeed
-
-# Transitional names retained while callers are migrated --------------------
-from .boolean_input import BooleanInput
-from .choice_input import ChoiceInput
-from .integer_input import IntegerInput
-from .number_input import NumberInput
-from .text_input import TextInput
 
 __all__ = [
     "InputFormInteger",
@@ -24,9 +16,4 @@ __all__ = [
     "InputMatrixNumber",
     "InputSearch",
     "InputTimeManagerSpeed",
-    "BooleanInput",
-    "ChoiceInput",
-    "IntegerInput",
-    "NumberInput",
-    "TextInput",
 ]
