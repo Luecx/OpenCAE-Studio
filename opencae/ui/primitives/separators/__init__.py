0b899e18e644c60bcfc2c4456545b28a342aa990
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-14 21:02:17 +0200

    Export Results ribbon separator

--- opencae/ui/primitives/separators/__init__.py
+++ opencae/ui/primitives/separators/__init__.py
@@ -1,6 +1,11 @@
 """Visual separator primitives."""
 
 from .separator_horizontal import SeparatorHorizontal
+from .separator_results_ribbon import SeparatorResultsRibbon
 from .separator_vertical import SeparatorVertical
 
-__all__ = ["SeparatorHorizontal", "SeparatorVertical"]
+__all__ = [
+    "SeparatorHorizontal",
+    "SeparatorResultsRibbon",
+    "SeparatorVertical",
+]
