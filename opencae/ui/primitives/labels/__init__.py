5d778131736eb93d3b4f8a845a024bd0ff72a37a
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-14 20:05:39 +0200

    Add label primitives package

---
+++ opencae/ui/primitives/labels/__init__.py
@@ -0,0 +1,15 @@
+"""Concrete text primitives used throughout OpenCAE."""
+
+from .label_body import LabelBody
+from .label_form import LabelForm
+from .label_muted import LabelMuted
+from .label_section import LabelSection
+from .label_status import LabelStatus
+
+__all__ = [
+    "LabelBody",
+    "LabelForm",
+    "LabelMuted",
+    "LabelSection",
+    "LabelStatus",
+]
