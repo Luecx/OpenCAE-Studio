c163f64120ade8d196470f9960fe9fc0d33c6b05
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-14 20:06:50 +0200

    Add separator primitives package

---
+++ opencae/ui/primitives/separators/__init__.py
@@ -0,0 +1,6 @@
+"""Visual separator primitives."""
+
+from .separator_horizontal import SeparatorHorizontal
+from .separator_vertical import SeparatorVertical
+
+__all__ = ["SeparatorHorizontal", "SeparatorVertical"]
