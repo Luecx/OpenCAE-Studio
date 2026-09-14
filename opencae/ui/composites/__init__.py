aaf89cdd615e266c8809b3c3e035ced710f55e27
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-14 19:42:43 +0200

    Add reusable UI composite package

---
+++ opencae/ui/composites/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable UI compositions built exclusively from canonical primitives."""
+
+from .collapsed_action_group import CollapsedActionGroupButton
+
+__all__ = ["CollapsedActionGroupButton"]
