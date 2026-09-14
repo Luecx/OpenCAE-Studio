a52b4f0d8c71c65ef41562a3aa16d5679e8a55d1
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-14 20:31:22 +0200

    Export matrix header label primitive

--- opencae/ui/primitives/labels/__init__.py
+++ opencae/ui/primitives/labels/__init__.py
@@ -2,6 +2,7 @@
 
 from .label_body import LabelBody
 from .label_form import LabelForm
+from .label_matrix_header import LabelMatrixHeader
 from .label_muted import LabelMuted
 from .label_section import LabelSection
 from .label_status import LabelStatus
@@ -9,6 +10,7 @@ from .label_status import LabelStatus
 __all__ = [
     "LabelBody",
     "LabelForm",
+    "LabelMatrixHeader",
     "LabelMuted",
     "LabelSection",
     "LabelStatus",
