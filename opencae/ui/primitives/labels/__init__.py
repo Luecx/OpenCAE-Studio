87b8a56ab6efcfaa52a321760a2209bc3cd4cadb
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-14 20:45:13 +0200

    Export title and group label primitives

--- opencae/ui/primitives/labels/__init__.py
+++ opencae/ui/primitives/labels/__init__.py
@@ -2,16 +2,20 @@
 
 from .label_body import LabelBody
 from .label_form import LabelForm
+from .label_group import LabelGroup
 from .label_matrix_header import LabelMatrixHeader
 from .label_muted import LabelMuted
 from .label_section import LabelSection
 from .label_status import LabelStatus
+from .label_title import LabelTitle
 
 __all__ = [
     "LabelBody",
     "LabelForm",
+    "LabelGroup",
     "LabelMatrixHeader",
     "LabelMuted",
     "LabelSection",
     "LabelStatus",
+    "LabelTitle",
 ]
